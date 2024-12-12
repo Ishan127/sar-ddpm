cbea43ab01b27aabdcc9f378d388598bfbe91944
Ishan127 <164486869+Ishan-Deepsolv@users.noreply.github.com>
2024-12-12 17:48:25 +0530

    fix

--- scripts/sarddpm_train.py
+++ scripts/sarddpm_train.py
@@ -6,7 +6,7 @@ import argparse
 
 import torch.nn.functional as F
 
-from guided_diffusion import dist_util, logger
+from guided_diffusion import logger
 from guided_diffusion.image_datasets import load_data
 from guided_diffusion.resample import create_named_schedule_sampler
 from guided_diffusion.script_util import (
