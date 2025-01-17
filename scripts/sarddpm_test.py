79f27077618746917bf4562df4f423f812fef28b
vishrutgrover <grovervishrut@gmail.com>
2025-01-17 12:42:51 +0530

    float krlo

--- scripts/sarddpm_test.py
+++ scripts/sarddpm_test.py
@@ -51,8 +51,12 @@ def main():
     model_clean.load_state_dict(torch.load(resume_checkpoint_clean, map_location=device))
 
     model_clean.to(device)
+
     if args.use_fp16:
         model_clean.half()
+    else:
+        model_clean.float()
+
 
     params =  list(model_clean.parameters())
 
@@ -85,22 +89,25 @@ def main():
                     val_inputv[:,:,:row,col:] = single_img[:,:,max_r-row:,:max_c-col]
 
                     model_kwargs = {}
-                    for k, v in model_kwargs1.items():
-                        if('Index' in k):
-                            img_name=v
-                        elif('SR' in k):
-                            model_kwargs[k] = val_inputv.to(dist_util.dev())
-                        else:
-                            model_kwargs[k]= v.to(dist_util.dev())
-
                     val_inputv = val_inputv.half() if args.use_fp16 else val_inputv.float()
-                    model_kwargs[k] = model_kwargs[k].half() if args.use_fp16 else model_kwargs[k].float()
+                    for k, v in model_kwargs1.items():
+                        if 'Index' in k:
+                            img_name = v
+                        elif 'SR' in k:
+                            model_kwargs[k] = val_inputv
+                        else:
+                            model_kwargs[k] = v.half() if args.use_fp16 else v.float()
+
                     sample = diffusion.p_sample_loop(
-                                    model_clean,
-                                    (clean_batch.shape[0], 3, 256,256),
-                                    clip_denoised=True,
-                                    model_kwargs=model_kwargs,
-                                )
+                        model_clean,
+                        (clean_batch.shape[0], 3, 256, 256),
+                        clip_denoised=True,
+                        model_kwargs=model_kwargs,
+                    )
+                    if args.use_fp16:
+                        sample = sample.half()
+                    else:
+                        sample = sample.float()
 
                     if count==0:
                         sample_new = (1.0/N)*sample
