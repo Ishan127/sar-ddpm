cd34b5c9a1970141c68abfbd6ebdb7d5bfa89a56
vishrutgrover <grovervishrut@gmail.com>
2025-01-17 11:53:22 +0530

    oops

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -426,8 +426,8 @@ class GaussianDiffusion:
                  - 'sample': a random sample from the model.
                  - 'pred_xstart': a prediction of x_0.
         """
-        x = x.half() if model.dtype == torch.float16 else x
-        t = t.half() if model.dtype == torch.float16 else t
+        x = x.half() if model.dtype == th.float16 else x
+        t = t.half() if model.dtype == th.float16 else t
         model_kwargs["SR"] = model_kwargs["SR"].half() if model.dtype == torch.float16 else model_kwargs["SR"]
 
         x_disto_start =  model_kwargs["SR"]
