d5df032ea026f5384b5f772f0073ac85ee925e46
vishrutgrover <grovervishrut@gmail.com>
2025-01-19 22:57:18 +0530

    try except

--- scripts/sarddpm_test.py
+++ scripts/sarddpm_test.py
@@ -63,77 +63,79 @@ def main():
 
     with torch.no_grad(): 
         number = 0
+        try:
+            for batch_id1, data_var in enumerate(val_data):
+                number = number+1 
+                clean_batch, model_kwargs1 = data_var
 
-        for batch_id1, data_var in enumerate(val_data):
-            number = number+1 
-            clean_batch, model_kwargs1 = data_var
+                single_img = model_kwargs1['SR'].to(dist_util.dev())
 
-            single_img = model_kwargs1['SR'].to(dist_util.dev())
+                count = 0
+                [t1,t2,max_r,max_c] = single_img.size()
+                
+                N = 9
+                
+                val_inputv = single_img.clone()
 
-            count = 0
-            [t1,t2,max_r,max_c] = single_img.size()
-            
-            N = 9
-            
-            val_inputv = single_img.clone()
-
-            
-            # for row in range(0,max_r,100):
-            #     for col in range(0,max_c,100):
-                    
-            #         val_inputv[:,:,:row,:col] = single_img[:,:,max_r-row:,max_c-col:]
-            #         val_inputv[:,:,row:,col:] = single_img[:,:,:max_r-row,:max_c-col]
-            #         val_inputv[:,:,row:,:col] = single_img[:,:,:max_r-row,max_c-col:]
-            #         val_inputv[:,:,:row,col:] = single_img[:,:,max_r-row:,:max_c-col]
-
-            model_kwargs = {}
-            val_inputv = val_inputv.half() if args.use_fp16 else val_inputv.float()
-            for k, v in model_kwargs1.items():
-                if "Index" in k:
-                    img_name = v
-                elif "SR" in k:
-                    model_kwargs[k] = val_inputv
-                else:
-                    model_kwargs[k] = v.half() if args.use_fp16 else v.float()
-
-            with torch.amp.autocast(device_type='cuda', enabled=args.use_fp16):  # Apply autocast here
-                sample = diffusion.p_sample_loop(
-                    model_clean,
-                    (clean_batch.shape[0], 3, 256, 256),
-                    clip_denoised=True,
-                    model_kwargs=model_kwargs,
-                    device=device,
-                    progress=True,
-                )
-
-            if args.use_fp16:
-                sample = sample.half()
-            else:
-                sample = sample.float()
-
-                    # if count==0:
-                    #     sample_new = (1.0/N)*sample
-                    # else : 
-                    #     sample_new[:,:,max_r-row:,max_c-col:] = sample_new[:,:,max_r-row:,max_c-col:] + (1.0/N)*sample[:,:,:row,:col]
-                    #     sample_new[:,:,:max_r-row,:max_c-col] = sample_new[:,:,:max_r-row,:max_c-col] + (1.0/N)*sample[:,:,row:,col:]
-                    #     sample_new[:,:,:max_r-row,max_c-col:] = sample_new[:,:,:max_r-row,max_c-col:] + (1.0/N)*sample[:,:,row:,:col]
-                    #     sample_new[:,:,max_r-row:,:max_c-col] = sample_new[:,:,max_r-row:,:max_c-col] + (1.0/N)*sample[:,:,:row,col:]
+                
+                # for row in range(0,max_r,100):
+                #     for col in range(0,max_c,100):
                         
-                    # count += 1
+                #         val_inputv[:,:,:row,:col] = single_img[:,:,max_r-row:,max_c-col:]
+                #         val_inputv[:,:,row:,col:] = single_img[:,:,:max_r-row,:max_c-col]
+                #         val_inputv[:,:,row:,:col] = single_img[:,:,:max_r-row,max_c-col:]
+                #         val_inputv[:,:,:row,col:] = single_img[:,:,max_r-row:,:max_c-col]
 
-            sample_new = ((sample + 1) * 127.5)
-            sample_new = sample_new.clamp(0, 255).to(torch.uint8)
-            sample_new = sample_new.permute(0, 2, 3, 1)
-            sample_new = sample_new.contiguous().cpu().numpy() # might have to put to CUDAs
-            
-            for i in range(sample_new.shape[0]):
-                try:
-                    snew = sample_new[i][:,:,::-1]
-                    snew = cv2.cvtColor(snew, cv2.COLOR_BGR2GRAY)
-                    print(img_name[i])
-                    cv2.imwrite(base_path+'pred_'+img_name[i],snew)
-                except Exception as E:
-                    print("Error in saving image", img_name[i], E)
+                model_kwargs = {}
+                val_inputv = val_inputv.half() if args.use_fp16 else val_inputv.float()
+                for k, v in model_kwargs1.items():
+                    if "Index" in k:
+                        img_name = v
+                    elif "SR" in k:
+                        model_kwargs[k] = val_inputv
+                    else:
+                        model_kwargs[k] = v.half() if args.use_fp16 else v.float()
+
+                with torch.amp.autocast(device_type='cuda', enabled=args.use_fp16):  # Apply autocast here
+                    sample = diffusion.p_sample_loop(
+                        model_clean,
+                        (clean_batch.shape[0], 3, 256, 256),
+                        clip_denoised=True,
+                        model_kwargs=model_kwargs,
+                        device=device,
+                        progress=True,
+                    )
+
+                if args.use_fp16:
+                    sample = sample.half()
+                else:
+                    sample = sample.float()
+
+                        # if count==0:
+                        #     sample_new = (1.0/N)*sample
+                        # else : 
+                        #     sample_new[:,:,max_r-row:,max_c-col:] = sample_new[:,:,max_r-row:,max_c-col:] + (1.0/N)*sample[:,:,:row,:col]
+                        #     sample_new[:,:,:max_r-row,:max_c-col] = sample_new[:,:,:max_r-row,:max_c-col] + (1.0/N)*sample[:,:,row:,col:]
+                        #     sample_new[:,:,:max_r-row,max_c-col:] = sample_new[:,:,:max_r-row,max_c-col:] + (1.0/N)*sample[:,:,row:,:col]
+                        #     sample_new[:,:,max_r-row:,:max_c-col] = sample_new[:,:,max_r-row:,:max_c-col] + (1.0/N)*sample[:,:,:row,col:]
+                            
+                        # count += 1
+
+                sample_new = ((sample + 1) * 127.5)
+                sample_new = sample_new.clamp(0, 255).to(torch.uint8)
+                sample_new = sample_new.permute(0, 2, 3, 1)
+                sample_new = sample_new.contiguous().cpu().numpy() # might have to put to CUDAs
+                
+                for i in range(sample_new.shape[0]):
+                    try:
+                        snew = sample_new[i][:,:,::-1]
+                        snew = cv2.cvtColor(snew, cv2.COLOR_BGR2GRAY)
+                        print(img_name[i])
+                        cv2.imwrite(base_path+'pred_'+img_name[i],snew)
+                    except Exception as E:
+                        print("Error in saving image", img_name[i], E)
+        except Exception as E:
+            print("Error: ", E)
 
 def create_argparser():
     defaults = dict(
